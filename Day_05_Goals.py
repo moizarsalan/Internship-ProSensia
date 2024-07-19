8e5c6781da308a10acf684ff793f9648005e4af2
Abdul Moiz Arsalan <B22F1086AI072@fecid.paf-iast.edu.pk>
2024-07-19 12:08:59 +0500

    Create Day_05_Goals.py

---
+++ Day_05_Goals.py
@@ -0,0 +1,37 @@
+print (" ---->Day 05 of Internship<---- ")
+
+print (" \n ---->Introduction and primary Goal of my life ")
+
+print ("""\nHello my name is Abdul Moiz Arsalan. 
+       The primary goal of my life is to be an expert AI developer, 
+       Artificial intelligence is a very interesting field yet challanging. """)
+
+print (" \n----> Importance of this goal ")
+
+print (""" \nThe goal is important to me because I have always wanted to be in AI since I was in college. Artificial intelligence
+       is a field of surprises it surprieses you many ways. Making the technology intereact with the envoirment is always interesting.
+       """)
+
+print (" \n----> Steps to achieve this goal ")
+
+print ("""\n 
+           1. Learning the requ ired skills for AI
+           2. Practicing the skills that I have learned for AI
+           3. Working on basic projects
+           4. After working on Basic projects I will have enough experience to work with advance projects
+           5. Implementing my skills in freelancing
+           6. Working with different project and different peoples in the fields
+           7. Working different corporate sectors
+       Following these steps I can be an expert in the field of AI""")
+
+print ("\n ----> Challanges that I can face ")
+
+print ("""\n The most important challange that I can face is tough routine time because there is a lot to cover with daiy life.
+       as I am also studying so I have to handle my studies as well as practice my skills that I am leaning with my studies. 
+       The only way to overcome this challange is to follow my daily schedule, be consistant and never backdown.""")
+
+print ("""\n Success to me means more than just becoming skilled in AI development; 
+       it's about making a real impact. I see a future where my work in AI improves daily life, simplifies tasks, and offers new solutions to global problems. 
+       To become an expert, I need to keep learning, growing, and exploring new possibilities. This also involves working with others, sharing knowledge, 
+       and motivating future AI developers. 
+       In the end, success will be measured by the positive changes I bring to the world through AI, making it a smarter and better place. """)
